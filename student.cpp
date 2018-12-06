df2b8a2e912f05d067a25632618b0cd60203f97d
Eperez8 <eperez8@ecc-jaguar.csuchico.edu>
2018-12-05 16:36:14 -0800

    StudentManagerAdvanced

--- student.cpp
+++ student.cpp
@@ -1,6 +1,13 @@
 #include "student.h"
 #include <iostream>
 #include <string>
+#include <vector>
+
+student::student()
+{
+    first = "";
+    last = "";
+}
 
 void student::setName(string firstName,string lastName)
 {
@@ -15,3 +22,30 @@ string student::fullName()
     name.append(last);
     return name;
 }
+
+double student::addGrade(double grade)
+{
+    gradeList.push_back(grade);
+}
+
+double student::getScore()
+{
+    double numerator = 0.0;
+    double score = 0.0;
+
+    for(int i = 0; i < gradeList.size(); i++)
+    {
+        numerator += gradeList[i];
+    }
+
+    if( gradeList.size() == 0)
+    {
+        score = 0;
+    }
+    else
+    {
+        score = numerator/gradeList.size();
+    }
+    return score;
+}
+
--- student.h
+++ student.h
@@ -1,22 +1,26 @@
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class student
 {
 
-private:
-string first;
-string last;
+    private:
+    string first;
+    string last;
+    vector<double> gradeList;
 
-public:
-void setName(string,string);
-string fullName();
-
+    public:
+    void setName(string,string);
+    string fullName();
+    double getScore();
+    double addGrade(double);
+    student();
 };
 
-#include "student.h"
 
 #endif
+
